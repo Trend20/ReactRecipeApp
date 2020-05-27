10633a32afe6fe7edd4235f4437ed065a079b926
Trend20 <enockomondi305@gmail.com>
2020-05-27 14:46:24 -0400

    new recipe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 
 class Navbar extends Component{
     render(){
+        const { newRecipe } = this.props;
         return(
             <header>
                 <h2><a>Recipe App</a></h2>
                 <nav>
-                    <li><a>New Recipe</a></li>
+                    <li><a onClick={newRecipe}>New Recipe</a></li>
                     <li><a>Home</a></li>
                     <li><a>About</a></li>
                     <li><a>Contact Us</a></li>
