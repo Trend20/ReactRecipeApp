9fed93bfaabe786616ec8a29e2ac2ec7277d16e8
Trend20 <enockomondi305@gmail.com>
2020-05-28 12:45:17 -0400

    recipelist commit

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
- 
-class RecipeList extends Component {
-    render() { 
-        return (
-            <div>
-                RecipeList
-            </div>
-        );
-    }
-}
- 
+import React, {Component} from 'react';
+import Recipe from './Recipe';
+
+const RecipeList = ({ recipes }) => {
+  recipes = recipes.map((r,index) => (
+    <Recipe key={index} {...r} />
+  ));
+  
+  return (
+    <div className="recipe-list">
+      {recipes}
+    </div>
+  );
+};
+
 export default RecipeList;
