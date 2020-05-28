6ca0edc513fe9f59e16da58e7fbaeff5ebd47e4b
Trend20 <enockomondi305@gmail.com>
2020-05-28 12:37:32 -0400

    correct

--- src/components/RecipeForm.js
+++ src/components/RecipeForm.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import styles from 'RecipeForm.css';
  
-class RecipeForm extends Component {
+class RecipeForm extends Component {    
+    constructor(props) {
+        super(props);
+        this.state = {
+          title: '',
+          instructions: '',
+          ingredients: [''],
+          img: ''
+        };
+      }
+    
+      clearForm = () => {
+        this.setState({
+          title: '',
+          instructions: '',
+          ingredients: [''],
+          img: ''
+        });
+      }
     render() { 
         return (
             <div>
