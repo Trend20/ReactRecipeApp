5144467778024a1572fb391ff5c761f2c9200dfc
Trend20 <enockomondi305@gmail.com>
2020-05-28 11:52:42 -0400

    maping ingredients

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,6 +4,11 @@ import PropTypes from 'prop-types';
 class Recipe extends Component {
     render() { 
         const { title, img, instructions } = this.props;
+        const ingredients = this.props.ingredients.map((ing, index) =>(
+            <li key={index}>
+                {ing}
+            </li>
+        ));
         return (
             <div>
                 Recipe
