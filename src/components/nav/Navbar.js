cd28f076221c10117d6171c4f134370c1a74207f
Trend20 <enockomondi305@gmail.com>
2020-05-31 09:10:45 -0400

    router dependencies

--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class Navbar extends Component{
