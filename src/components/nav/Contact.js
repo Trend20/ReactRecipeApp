ebdfaabe43681db2cf418b62afad5723bdaf25d1
Trend20 <enockomondi305@gmail.com>
2020-06-01 16:08:19 -0400

    state declaration

--- src/components/nav/Contact.js
+++ src/components/nav/Contact.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
  
 class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            username: '',
+            email:'',
+            message:[],
+        }
+    }
     render() { 
         return (
             <div className="contact">
