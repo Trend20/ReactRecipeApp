759fca6e8ca704573101fe753ada6efbb2216808
Trend20 <enockomondi305@gmail.com>
2020-06-02 12:01:43 -0400

    email change

--- src/components/nav/Contact.js
+++ src/components/nav/Contact.js
@@ -37,18 +37,22 @@ class Contact extends Component {
             <div className="contact">
                 <form id="contact-form" method="POST">
                    <h3 className="text-center text-secondary">Contact Us</h3>
+                  <div className="form-group">
                   <input 
                   type="text"
                   placeholder="Enter Username"
                   value={this.state.username}
                   onChange={this.handleUsernameInput.bind(this)}
                   />
+                  </div>
+                <div className="form-group">
                 <input
                 type="email"
                 placeholder="Enter email"
                 onChange={this.handleEmailInput.bind(this)}
                 value={this.state.email}
                 />
+                </div>
                 <textarea
                 placeholder="Message"
                 value={this.state.message}
