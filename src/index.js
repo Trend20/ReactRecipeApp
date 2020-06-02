0840c017260ea45181e954e8bf576f7d0f5dab06
Trend20 <enockomondi305@gmail.com>
2020-06-02 12:30:25 -0400

    components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from './components/nav/Navbar';
-// import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Navbar />
   </React.StrictMode>,
   document.getElementById('root')
