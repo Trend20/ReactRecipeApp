fc25f2c1f058029d04342d10e87de3bcd1507adc
Trend20 <enockomondi305@gmail.com>
2020-06-02 14:34:44 -0400

    error

--- src/server.js
+++ src/server.js
@@ -16,3 +16,11 @@ const transport = {
 }
 
 let transporter = nodemailer.createTransport(transport);
+
+transporter.verify((error, success) =>{
+    if(error){
+        console.log(error);
+    }else {
+        console.log('Server is ready to take messages');
+    }
+});
