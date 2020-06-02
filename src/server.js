2a088cdd8490c05876e4e1bef58c8cf373a21eb0
Trend20 <enockomondi305@gmail.com>
2020-06-02 14:31:12 -0400

    transport

--- src/server.js
+++ src/server.js
@@ -3,3 +3,14 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const creds = require('./config');
+
+
+
+const transport = {
+    host: 'smtp.example.com',
+    port: '587',
+    auth:{
+        user: creds.USER,
+        pass: creds.PASS,
+    }
+}
